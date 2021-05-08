55756551c530fdafd4251f44b43e8b951ba92f9c
Emil VATAI <emil.vatai@gmail.com>
2021-05-08 19:00:16 +0900

    Corpus summary

--- nlp/main.py
+++ nlp/main.py
@@ -1,14 +1,35 @@
-import vecto
+import vecto.corpus
 
-from util import head
+
+def head(get_iter, n=10):
+    count = 0
+    for itr in get_iter:
+        print(itr)
+        count += 1
+        if count == n:
+            break
+
+
+def section(path, fun, args=[]):
+    print(f"# {fun}")
+    corpus = vecto.corpus.DirCorpus(path)
+    get_iter = eval(f"corpus.{fun}")
+    print("```")
+    head(get_iter(*args))
+    print("```")
+    print()
 
 
 def main():
     path = "../data/nlp"
-    corpus = vecto.corpus.DirCorpus(path)
-    print("hello")
+    tokenizer = vecto.corpus.tokenization.DEFAULT_TOKENIZER
+    sequence_length = 4
 
-    head(corpus.get_line_iterator)
+    section(path, "get_token_iterator", args=[tokenizer])
+    section(path, "get_line_iterator")
+    section(path, "get_sentence_iterator", args=[tokenizer])
+    section(path, "get_sequence_iterator", args=[sequence_length, tokenizer])
+    section(path, "get_sliding_window_iterator", args=[2, 3, tokenizer])
 
 
 if __name__ == "__main__":
