be3f54668ee03b0f0881b05f5a8f5aa4814df4f7
Emil VATAI <emil.vatai@gmail.com>
2021-05-10 11:22:07 +0900

    Checked Id of word

--- nlp/main.py
+++ nlp/main.py
@@ -9,6 +9,10 @@ def main():
 
     print(v.lst_words[:10])
 
+    for word in ["one", "said", "like"]:
+        idx = v.get_id(word)
+        print(f"index of {word}: {idx}")
+
 
 if __name__ == "__main__":
     main()
