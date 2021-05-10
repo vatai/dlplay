1638798881529103388289ef86d75800adbe86c1
Emil VATAI <emil.vatai@gmail.com>
2021-05-10 18:58:36 +0900

    Generated new vocab

--- nlp/main.py
+++ nlp/main.py
@@ -4,7 +4,7 @@ import vecto.vocabulary
 def main():
     v = vecto.vocabulary.Vocabulary()
 
-    path = "../data/nlp/vocabs/bnc/m10/"
+    path = "../data/nlp/corpora/BNC/m10/normal"
     v.load(path)
 
     print(v.lst_words[:10])
@@ -12,6 +12,11 @@ def main():
     for word in ["one", "said", "like"]:
         idx = v.get_id(word)
         print(f"index of {word}: {idx}")
+
+    word = "megszentsegtelenethetetleneskedeseitekert"
+    idx = v.get_id(word)
+    print(f"index of {word}: {idx}")
+    print(len(word))
 
 
 if __name__ == "__main__":
