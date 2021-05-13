d47c647d7af6e7f5e342895b30d46beb24ab1dad
Emil VATAI <emil.vatai@gmail.com>
2021-05-13 22:50:29 +0900

    Start training part

--- nlp/word2vec.py
+++ nlp/word2vec.py
@@ -1,14 +1,18 @@
 import argparse
 from pathlib import Path
 
+from torch import nn
 from vecto.corpus import DirCorpus, tokenization
 from vecto.vocabulary import Vocabulary
 
-# import torch
+
+def get_model(args, cnt_words):
+    embeddings = nn.Embedding(cnt_words, args.embed_width)
+    return embeddings
 
 
-# def mkmodel():
-#     pass
+def train(model, corpus, vocab):
+    pass
 
 
 def main(args):
@@ -16,6 +20,7 @@ def main(args):
     corpus = DirCorpus(args.path)
     vocab = Vocabulary()
     tokenizer = tokenization.DEFAULT_TOKENIZER
+
     vocab.load(args.path / "m10/normal")
     count = 0
     for itr in corpus.get_sliding_window_iterator(tokenizer=tokenizer):
@@ -28,14 +33,14 @@ def main(args):
         if count == 10:
             break
 
+    model = get_model(args, vocab.cnt_words)
+    train(model, corpus, vocab)
+
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--path",
-        type=Path,
-        default=Path("../data/nlp/corpora/BNC"),
-    )
+    parser.add_argument("--path", type=Path, default=Path("../data/nlp/corpora/BNC"))
+    parser.add_argument("--embed-width", type=int, default=512)
     return parser.parse_args()
 
 
