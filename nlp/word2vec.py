d62d30e526a7bbce44df17520c0c4e98fa7c5a14
Emil VATAI <emil.vatai@gmail.com>
2021-05-13 22:22:21 +0900

    Start word2vec

---
+++ nlp/word2vec.py
@@ -0,0 +1,41 @@
+import argparse
+from pathlib import Path
+
+from vecto.corpus import DirCorpus, tokenization
+from vecto.vocabulary import Vocabulary
+
+# import torch
+
+
+# def mkmodel():
+#     pass
+
+
+def main(args):
+    print("start word2vec")
+    corpus = DirCorpus(args.path)
+    vocab = Vocabulary()
+    tokenizer = tokenization.DEFAULT_TOKENIZER
+    vocab.load(args.path / "m10/normal")
+    count = 0
+    for itr in corpus.get_sliding_window_iterator(tokenizer=tokenizer):
+        cur, ctx = itr["current"], itr["context"]
+        print(cur, ctx)
+        count += 1
+        if count == 10:
+            break
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        type=Path,
+        default=Path("../data/nlp/corpora/BNC"),
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
