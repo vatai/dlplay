d96839274aaddd9e3338387783a107a42add5b14
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 22:06:51 +0900

    Try larger lr

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -1,3 +1,4 @@
 # git pull
-# python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100 
-python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100 
+# python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100
+# python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100
+python word2vec.py --corpus-path --lr=0.01 --max-lr=0.01 ../BNC/AA --vocab-path ../BNC/m100
