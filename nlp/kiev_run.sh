3358e97ee5b3ab25d511d952186da18b73bb533a
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 21:57:17 +0900

    Use BNC dataset

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -1,2 +1,3 @@
 git pull
-python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100 
+# python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100 
+python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100 
