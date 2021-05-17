019a75fe8f62525487740d4c337f3e14978107ad
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 22:48:52 +0900

    Try up down

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -1,8 +1,10 @@
 # git pull
 # python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100
 # python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100
-# python word2vec.py --corpus-path=../BNC/AA --lr=0.01 --max-lr=0.01 --vocab-path ../BNC/m100
-# python word2vec.py --corpus-path=../BNC/AA --lr=0.005 --max-lr=0.005 --vocab-path ../BNC/m100
-# python word2vec.py --corpus-path=../BNC/AA --lr=0.001 --max-lr=0.001 --vocab-path ../BNC/m100
+# python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.01  --max-lr=0.01
+# python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.005 --max-lr=0.005
+# python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.001
 # python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.001 --momentum=0.5
-python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.005 --momentum=0.5
+# python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.005 --momentum=0.5
+
+python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.05 --pct-start=0.1 --momentum=0.5
