463bc1f62c161ab4ded2a4a724345f77b3f70911
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 22:00:04 +0900

    Remove `git pull` from `kiev_run.sh`

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -1,3 +1,3 @@
-git pull
+# git pull
 # python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100 
 python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100 
