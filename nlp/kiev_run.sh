c8add293364b3eab03eec1c0bbcb32eab85a7a95
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 22:27:24 +0900

    Add momentum

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -3,4 +3,5 @@
 # python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100
 # python word2vec.py --corpus-path=../BNC/AA --lr=0.01 --max-lr=0.01 --vocab-path ../BNC/m100
 # python word2vec.py --corpus-path=../BNC/AA --lr=0.005 --max-lr=0.005 --vocab-path ../BNC/m100
-python word2vec.py --corpus-path=../BNC/AA --lr=0.001 --max-lr=0.001 --vocab-path ../BNC/m100
+# python word2vec.py --corpus-path=../BNC/AA --lr=0.001 --max-lr=0.001 --vocab-path ../BNC/m100
+python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.001 --momentum=0.5
