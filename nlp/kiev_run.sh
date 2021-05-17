a5e5612f57a90e36024361098f8dca3630ad8019
Emil VATAI <emil.vatai@gmail.com>
2021-05-17 22:14:07 +0900

    Add another `lr`

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -1,4 +1,5 @@
 # git pull
 # python word2vec.py --corpus-path ../enwiki_2021.01/AA --vocab-path ../enwiki_2021.01/m100
 # python word2vec.py --corpus-path ../BNC/AA --vocab-path ../BNC/m100
-python word2vec.py --corpus-path=../BNC/AA --lr=0.01 --max-lr=0.01 --vocab-path ../BNC/m100
+# python word2vec.py --corpus-path=../BNC/AA --lr=0.01 --max-lr=0.01 --vocab-path ../BNC/m100
+python word2vec.py --corpus-path=../BNC/AA --lr=0.005 --max-lr=0.005 --vocab-path ../BNC/m100
