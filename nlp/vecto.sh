4c1707f820cb35f8b63e73bf68d71e4451c19222
Emil VATAI <emil.vatai@gmail.com>
2021-05-21 10:36:16 +0900

    Add `vecto.sh`

---
+++ nlp/vecto.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set_env /work/opt/cuda/nccl_2.7.8-1+cuda10.2_x86_64/
+set_env /work/opt/cuda/cudnn-10.0-linux-x64-v7.4.2.24/
+spack load gcc@8.4.0
+python -m vecto.embeddings.train_word2vec --gpu=0 --path_corpus=BNC/AA/ --path_out=vecto_w2v
