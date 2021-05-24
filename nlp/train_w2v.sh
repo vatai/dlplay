92a8846c4a493b51c86f0f52acbb2c37d4be51c6
Emil VATAI <emil.vatai@gmail.com>
2021-05-24 22:49:30 +0900

    Add w2c training cmds from Alex

--- nlp/train_w2v.sh
+++ nlp/train_w2v.sh
@@ -7,7 +7,14 @@ spack load gcc@8.4.0
 python -m \
 	vecto.embeddings.train_word2vec \
 	--gpu=0 \
+        --window=5 \
 	--epoch=10 \
 	--path_out=w2v \
 	--path_corpus=../BNC/AA/ \
         --path_vocab=../BNC/m100/normal
+
+# From alex
+# epoch=4
+# python3 -m vecto.embeddings.train_word2vec \
+#     --path_vocab=/work/data/NLP/vocabs/brown_m5 \
+#     --path_corpus=/work/data/NLP/corpora/raw_texts/Eng/BNC \
