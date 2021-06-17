52af212142ea5c1b1b751fffabd7807b00bff9cc
Emil VATAI <emil.vatai@gmail.com>
2021-06-17 11:33:44 +0900

    Right `steps-per-epoch` for BNC

--- nlp/kiev_run.sh
+++ nlp/kiev_run.sh
@@ -19,7 +19,7 @@
 
 # python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.05 --pct-start=0.1 --momentum=0.5 --steps-per-epoch=50000 --div-factor=25
 
-python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.05 --pct-start=0.1 --momentum=0.5 --steps-per-epoch=1000000 --div-factor=25 --embed-width=300
+python word2vec.py --corpus-path=../BNC/AA --vocab-path ../BNC/m100 --lr=0.001 --max-lr=0.05 --pct-start=0.1 --momentum=0.5 --steps-per-epoch=2000000 --div-factor=25 --embed-width=300
 
 mv last.chkp.npy tmp/normal/
 
